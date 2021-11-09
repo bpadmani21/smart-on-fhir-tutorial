3bb4490f0b7457f37cec8bda9b5051f16fb91897
Padmani, Bhavesh <bhavesh_padmani@baxter.com>
2021-11-09 14:23:31 -0600

    logging

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,6 +8,7 @@
     }
 
     function onReady(smart)  {
+      console.log('executing onReady. This is smart:');
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
